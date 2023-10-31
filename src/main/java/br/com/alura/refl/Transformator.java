59f9b5813679426b8a43b088e88451b0af99de21
Joao Victor <jvc.martins12@gmail.com>
2023-10-30 23:01:16 -0300

    codigo aula 2

--- src/main/java/br/com/alura/refl/Transformator.java
+++ src/main/java/br/com/alura/refl/Transformator.java
@@ -1,9 +1,19 @@
 package br.com.alura.refl;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
 public class Transformator {
 
-    public <I, O> transform(I input) throws ClassNotFoundException {
+    public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> source = input.getClass();
-        Class<?> target = source.forName(source.getClass() + "DTO");
+        Class<?> target = Class.forName(source + "DTO");
+
+        O targetClass = (O) target.getDeclaredConstructor().newInstance();
+
+        Field[] sourceFields = source.getDeclaredFields();
+        Field[] targetFields = target.getDeclaredFields();
+
+        return targetClass;
     }
 }
