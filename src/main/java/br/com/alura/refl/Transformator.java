b573bcbd8a16dbbdadaddc97eb2c9942885cb2a3
Joao Victor <jvc.martins12@gmail.com>
2023-10-31 22:28:05 -0300

    codigo da aula 3

--- src/main/java/br/com/alura/refl/Transformator.java
+++ src/main/java/br/com/alura/refl/Transformator.java
@@ -2,6 +2,7 @@ package br.com.alura.refl;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class Transformator {
 
@@ -14,6 +15,24 @@ public class Transformator {
         Field[] sourceFields = source.getDeclaredFields();
         Field[] targetFields = target.getDeclaredFields();
 
+        Arrays.stream(sourceFields).forEach(sourceField ->
+                Arrays.stream(targetFields).forEach(targetField -> {
+                    validate(sourceField, targetField);
+                    try {
+                        targetField.set(targetClass, sourceField.get(input));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }));
+
         return targetClass;
+    }
+
+    private void validate(Field sourceField, Field targetField) {
+        if (sourceField.getName().equals(targetField.getName())
+                && sourceField.getType().equals(targetField.getType())) {
+            sourceField.setAccessible(true);
+            targetField.setAccessible(true);
+        }
     }
 }
