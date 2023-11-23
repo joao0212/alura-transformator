cac16f5012c877b9c99b04fe29d7b9a8e3e20378
Joao Victor <jvc.martins12@gmail.com>
2023-11-22 23:54:04 -0300

    codigo inicial do curso

--- src/main/java/br/com/alura/refl/Transformator.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.refl;
-
-public class Transformator {
-
-    public <I, O> transform(I input) throws ClassNotFoundException {
-        Class<?> source = input.getClass();
-        Class<?> target = source.forName(source.getClass() + "DTO");
-    }
-}
